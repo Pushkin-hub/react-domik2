e4a1fa94cd1f3b1c7d2352d3b88d6d8e77f8f494
Pushkin-hub <pushkinanton2002@gmail.com>
2025-05-18 12:47:30 +0300

    Доработка домашки

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonalPage from './components/PersonalPage';
+import RegistrationForm from './components/RegistrationForm';
+import PhotoUploadForm from './components/PhotoUploadForm';
+import BandsList from './components/BandsList';
+import './components/CitiesList';
 
 function App() {
   return (
     <div>
-      
+      <PersonalPage />
+      <RegistrationForm />
+      <PhotoUploadForm />
+      <BandsList />
     </div>
   );
 }
